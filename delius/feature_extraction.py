f95c2b27773e1d3d25f18855b238e2fb1dbcb0a2
Gianmarco Turchiano <gianmarco.turchiano@outlook.it>
2025-02-23 18:28:19 +0100

    Implemented features extraction DVC stage.

---
+++ delius/feature_extraction.py
@@ -0,0 +1,130 @@
+import argparse
+from pathlib import Path
+from itertools import chain
+from PIL import Image
+
+import torch
+from torch.utils.data import DataLoader, Dataset
+from torchvision import transforms
+from torchvision.models import densenet121, DenseNet121_Weights
+from tqdm import tqdm
+
+
+class ArtDataset(Dataset):
+    def __init__(
+        self,
+        image_directory,
+        image_width=224,
+        image_height=224,
+        image_extensions=[
+            '*.jpg',
+            '*.jpeg',
+            '*.png',
+            '*.bmp',
+            '*.gif',
+            '*.tiff'
+        ]
+    ):
+        self.transform = transforms.Compose([
+            transforms.Resize((image_height, image_width)),
+            transforms.ToTensor(),
+            transforms.Lambda(lambda x: x / 255.0) # Min-Max normalization 
+        ])
+
+        image_directory_path = Path(image_directory)
+
+        if not image_directory_path.is_dir():
+            raise NotADirectoryError(f"'{image_directory}' is not a valid directory.")
+
+        self.image_paths = list(chain.from_iterable(image_directory_path.glob(ext) for ext in image_extensions))
+
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, idx):
+        img_path = self.image_paths[idx]
+        image = Image.open(img_path).convert('RGB')
+        image = self.transform(image)
+        return str(img_path), image
+    
+
+def get_densenet_embeddings(
+        image_directory,
+        image_width=224,
+        image_height=224,
+        batch_size=256
+):
+    tqdm.write(f"Reading '{image_directory}'...")
+
+    dataset = ArtDataset(
+        image_directory,
+        image_width,
+        image_height
+    )
+
+    loader = DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=False
+    )
+
+    tqdm.write(f'Loading model...')
+
+    model = densenet121(weights=DenseNet121_Weights.DEFAULT)
+    model.eval()
+
+    paths = []
+    embeddings = []
+
+    with torch.no_grad():
+        for path, image in tqdm(loader, desc='Extracting image features...'):
+            paths.extend(path)
+            embedding = model(image)
+            embeddings.extend(embedding)
+
+    return dict(zip(paths, embeddings))
+
+
+def save_densenet_embeddings(
+        image_directory,
+        output_file,
+        image_width=224,
+        image_height=224,
+        batch_size=256  
+):
+    embeddings = get_densenet_embeddings(
+        image_directory,
+        image_width,
+        image_height,
+        batch_size
+    )
+
+    tqdm.write(f"Saving embedding to file '{output_file}'...")
+
+    torch.save(embeddings, output_file)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--image_directory', type=str)
+    parser.add_argument('--output_file', type=str)
+    parser.add_argument('--image_width', type=int)
+    parser.add_argument('--image_height', type=int)
+    parser.add_argument('--batch_size', type=int)
+
+    args = parser.parse_args()
+
+    image_directory = args.image_directory
+    batch_size = args.batch_size
+    image_width = args.image_width
+    image_height = args.image_height
+    output_file = args.output_file
+
+    embeddings = save_densenet_embeddings(
+        image_directory,
+        output_file,
+        image_width,
+        image_height,
+        batch_size
+    )
